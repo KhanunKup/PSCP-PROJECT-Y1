b66bf39e41c80f360fc187eab948cfa8aef6c69a
Au4uztOLsen <pacharanatthakorn@gmail.com>
2024-10-22 22:30:51 +0700

    forms add to database success

--- database/mypage/views.py
+++ database/mypage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from mypage.models import creativereser
-from mypage.models import peerone
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -11,21 +12,34 @@ def creative(request):
         id_input = request.POST['student-id-form']
         name_input = request.POST['name-form']
         date_input = request.POST['date-form']
+        start_input = request.POST['start-date-form']
+        end_input = request.POST['end-date-form']
+
+        start_time = datetime.strptime(start_input, '%H:%M').time()
+        end_time = datetime.strptime(end_input, '%H:%M').time()
+
+        overlapping_bookings = creativereser.objects.filter(
+            event_date=date_input,
+            start_time__lt=end_time,
+            end_time__gt=start_time
+        ).exists()
+
+        if overlapping_bookings:
+            messages.error(request, "The room has been booked during that time. Please enter a different time slot!")
+            return redirect("creative.html")
 
         form = creativereser.objects.create(
-            student_id = id_input,
-            name = name_input,
-            event_date = date_input
+            student_id=id_input,
+            name=name_input,
+            event_date=date_input,
+            start_time=start_time,
+            end_time=end_time
         )
         form.save()
         return redirect("checkscreative.html")
     else:
-        return render(request,"creative.html")
+        return render(request, "creative.html")
 
 def room(request):
     list_person = creativereser.objects.all()
     return render(request,"checkscreative.html",{"list_person":list_person})
-
-def room_2(request):
-    list_person_2 = peerone.objects.all()
-    return render(request,"checkscreative.html",{"list_person_2":list_person_2})
