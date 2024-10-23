ff008e06f333a2fa45124ec174972be0c3ce3ea5
Thitiwut Khrutchuen. <khanun.krootchuen@gmail.com>
2024-10-23 12:07:00 +0700

    del database

--- database/mypage/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from mypage.models import creativereser
-
-# Register your models here.
-admin.site.register(creativereser)
--- database/mypage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from mypage import views
-
-urlpatterns = [
-    path('',views.index),
-    path('creative.html',views.creative),
-    path('index.html',views.index),
-    path('checkscreative.html',views.room)
-]
